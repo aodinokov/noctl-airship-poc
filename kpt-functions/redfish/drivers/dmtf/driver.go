33978ffafb1471a8c22bde250ed1cecbafb85be6
Alexey Odinokov <mralex@mail.ru>
2020-07-07 23:44:05 +0000

    made the init function

--- kpt-functions/redfish/drivers/dmtf/driver.go
+++ kpt-functions/redfish/drivers/dmtf/driver.go
@@ -13,11 +13,13 @@
 package dmtf
 
 import (
-	//"context"
+	"context"
 	"crypto/tls"
-	//"fmt"
+	"fmt"
 	"net/http"
-	//"strings"
+	"net/url"
+	"path"
+	"strings"
 	//"time"
 
 	redfishAPI "opendev.org/airship/go-redfish/api"
@@ -27,14 +29,62 @@ import (
 )
 
 type Driver struct {
-	Config *redfish.DriverConfig
-	Api redfishAPI.RedfishAPI
+	Config   *redfish.DriverConfig
+	Api      redfishAPI.RedfishAPI
+	SystemId string
+}
+
+func BasePath(url *url.URL) (string, error) {
+	var scheme string
+
+	// for possible options
+	// see https://github.com/metal3-io/baremetal-operator/blob/master/docs/api.md#spec-fields
+	switch url.Scheme {
+	case "redfish", "https", "redfish+https":
+		scheme = "https"
+	case "redfish+http", "http":
+		scheme = "http"
+	default:
+		return "", fmt.Errorf("the scheme %s isn't supported", url.Scheme)
+	}
+	return fmt.Sprintf("%s://%s", scheme, url.Host), nil
+}
+
+func ResourceId(url *url.URL, expectedPath string) (string, error) {
+	if expectedPath != "" {
+		if strings.TrimSuffix(expectedPath, "/") != path.Dir(strings.TrimSuffix(url.Path, "/")) {
+			return "", fmt.Errorf("Invalid resource type: expected %s, got %s",
+				strings.TrimSuffix(expectedPath, "/"),
+				path.Dir(strings.TrimSuffix(url.Path, "/")))
+		}
+	}
+	return path.Base(url.Path), nil
+}
+
+func SystemId(url *url.URL) (string, error) {
+	return ResourceId(url, "redfish/v1/Systems/")
 }
 
 func NewDriver(config *redfish.DriverConfig) (redfish.Driver, error) {
 	drv := Driver{Config: config}
 
+	url, err := url.Parse(config.BMC.URL)
+	if err != nil {
+		return nil, err
+	}
+
 	cfg := redfishClient.NewConfiguration()
+
+	cfg.BasePath, err = BasePath(url)
+	if err != nil {
+		return nil, err
+	}
+
+	drv.SystemId, err = SystemId(url)
+	if err != nil {
+		return nil, err
+	}
+
 	if config.UserAgent != nil {
 		cfg.UserAgent = *config.UserAgent
 	}
@@ -60,9 +110,7 @@ func NewDriver(config *redfish.DriverConfig) (redfish.Driver, error) {
 		Transport: transport,
 	}
 
-
 	drv.Api = redfishClient.NewAPIClient(cfg).DefaultApi
-
 	return &drv, nil
 }
 
@@ -84,4 +132,17 @@ func (d *Driver) EjectMedia() error {
 
 func (d *Driver) SetBootSource() error {
 	return nil
+}
+
+func (d *Driver) updateContext(ctx context.Context) context.Context {
+	if d.Config.BMC.Username != "" && d.Config.BMC.Password != "" {
+		ctx = context.WithValue(
+			ctx,
+			redfishClient.ContextBasicAuth,
+			redfishClient.BasicAuth{
+				UserName: d.Config.BMC.Username,
+				Password: d.Config.BMC.Password},
+		)
+	}
+	return ctx
 }
