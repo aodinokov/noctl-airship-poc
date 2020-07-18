e606c7bc40e4bdd542c04488f61994f7fbe3675a
Alexey Odinokov <mralex@mail.ru>
2020-07-18 04:51:54 +0000

    added template of the function

--- kpt-functions/replacement/function.go
+++ kpt-functions/replacement/function.go
@@ -3,12 +3,80 @@ package replacement
 import (
 	"fmt"
 
-	"sigs.k8s.io/kustomize/api/types"
 	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
+// Gvk identifies a Kubernetes API type.
+// https://github.com/kubernetes/community/blob/master/contributors/design-proposals/api-machinery/api-group.md
+type Gvk struct {
+	Group   string `json:"group,omitempty" yaml:"group,omitempty"`
+	Version string `json:"version,omitempty" yaml:"version,omitempty"`
+	Kind    string `json:"kind,omitempty" yaml:"kind,omitempty"`
+}
+
+// Selector specifies a set of resources.
+// Any resource that matches intersection of all conditions
+// is included in this set.
+type Selector struct {
+	Gvk       `json:",inline,omitempty" yaml:",inline,omitempty"`
+	Namespace string `json:"namespace,omitempty" yaml:"namespace,omitempty"`
+	Name      string `json:"name,omitempty" yaml:"name,omitempty"`
+
+	// AnnotationSelector is a string that follows the label selection expression
+	// https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#api
+	// It matches with the resource annotations.
+	AnnotationSelector string `json:"annotationSelector,omitempty" yaml:"annotationSelector,omitempty"`
+
+	// LabelSelector is a string that follows the label selection expression
+	// https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#api
+	// It matches with the resource labels.
+	LabelSelector string `json:"labelSelector,omitempty" yaml:"labelSelector,omitempty"`
+}
+
+// Target refers to a kubernetes object by Group, Version, Kind and Name
+// gvk.Gvk contains Group, Version and Kind
+// APIVersion is added to keep the backward compatibility of using ObjectReference
+// for Var.ObjRef
+type SourceObjRef struct {
+	APIVersion string `json:"apiVersion,omitempty" yaml:"apiVersion,omitempty"`
+	Gvk        `json:",inline,omitempty" yaml:",inline,omitempty"`
+	Name       string `json:"name" yaml:"name"`
+	Namespace  string `json:"namespace,omitempty" yaml:"namespace,omitempty"`
+}
+
+// Source defines where a substitution is from
+// It can from two different kinds of sources
+//  - from a field of one resource
+//  - from a several sources and go template
+//  - from a string
+type Source struct {
+	ObjRef   *SourceObjRef `json:"objref,omitempty" yaml:"objref,omitempty"`
+	FieldRef string        `json:"fieldref,omitempty" yaml:"fiedldref,omitempty"`
+
+	Value string `json:"value,omitempty" yaml:"value,omitempty"`
+
+	Multiref *struct {
+		Sources []struct {
+			ObjRef   *SourceObjRef `json:"objref" yaml:"objref"`
+			FieldRef string        `json:"fieldref" yaml:"fiedldref"`
+		}
+		Template string `json:"template" yaml:"template"`
+	} `json:"multiref,omitempty" yaml:"multiref,omitempty"`
+}
+
+// ReplTarget defines where a substitution is to.
+type Target struct {
+	ObjRef    *Selector `json:"objref,omitempty" yaml:"objref,omitempty"`
+	FieldRefs []string  `json:"fieldrefs,omitempty" yaml:"fieldrefs,omitempty"`
+}
+
+type Replacement struct {
+	Source *Source `json:"source" yaml:"source"`
+	Target *Target `json:"target" yaml:"target"`
+}
+
 type FunctionConfig struct {
-	Replacements []types.Replacement `json:"replacements,omitempty" yaml:"replacements,omitempty"`
+	Replacements []Replacement `json:"replacements,omitempty" yaml:"replacements,omitempty"`
 }
 
 type Function struct {
@@ -30,6 +98,9 @@ func NewFunction(cfg *FunctionConfig) (*Function, error) {
 		if r.Source.Value != "" {
 			count += 1
 		}
+		if r.Source.Multiref != nil {
+			count += 1
+		}
 		if count > 1 {
 			return nil, fmt.Errorf("only one of fieldref and value is allowed in one replacement")
 		}
@@ -41,25 +112,26 @@ func NewFunction(cfg *FunctionConfig) (*Function, error) {
 
 func (f *Function) Exec(items []*yaml.RNode) error {
 	for _, r := range f.Config.Replacements {
-		var replacement interface{}
-		if r.Source.ObjRef != nil {
-			// TODO: replacement, err = getReplacement(m, r.Source.ObjRef, r.Source.FieldRef)
-			//if err != nil {
-			//	return err
-			//}
-		}
-		if r.Source.Value != "" {
-			replacement = r.Source.Value
+		source, err := prepareSource(items, r.Source)
+		if err != nil {
+			return err
 		}
 
-		fmt.Printf("The replacement is %s\n", replacement)
+		fmt.Printf("The source is %s\n", source)
 
-		//err = substitute(m, r.Target, replacement)
-		//if err != nil {
-		//	return err
-		//}
+		err = apply(items, source, r.Target)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
+func prepareSource(items []*yaml.RNode, s *Source) (string, error) {
+//	if 
+	return "", nil
+}
 
+func apply(items []*yaml.RNode, source string, t *Target) error {
+	return nil
+}
