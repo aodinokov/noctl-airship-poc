27e13302b85d1ea25298a8baeaf0915cf764c9f9
Alexey Odinokov <mralex@mail.ru>
2020-07-18 02:40:54 +0000

    added more stub

--- kpt-functions/replacement/function.go
+++ kpt-functions/replacement/function.go
@@ -1,7 +1,7 @@
 package replacement
 
 import (
-
+	"fmt"
 
 	"sigs.k8s.io/kustomize/api/types"
 	"sigs.k8s.io/kustomize/kyaml/yaml"
@@ -16,10 +16,50 @@ type Function struct {
 }
 
 func NewFunction(cfg *FunctionConfig) (*Function, error) {
-	return nil, nil
+	for _, r := range cfg.Replacements {
+		if r.Source == nil {
+			return nil, fmt.Errorf("`from` must be specified in one replacement")
+		}
+		if r.Target == nil {
+			return nil, fmt.Errorf("`to` must be specified in one replacement")
+		}
+		count := 0
+		if r.Source.ObjRef != nil {
+			count += 1
+		}
+		if r.Source.Value != "" {
+			count += 1
+		}
+		if count > 1 {
+			return nil, fmt.Errorf("only one of fieldref and value is allowed in one replacement")
+		}
+	}
+
+	fn := Function{Config: cfg}
+	return &fn, nil
 }
 
 func (f *Function) Exec(items []*yaml.RNode) error {
+	for _, r := range f.Config.Replacements {
+		var replacement interface{}
+		if r.Source.ObjRef != nil {
+			// TODO: replacement, err = getReplacement(m, r.Source.ObjRef, r.Source.FieldRef)
+			//if err != nil {
+			//	return err
+			//}
+		}
+		if r.Source.Value != "" {
+			replacement = r.Source.Value
+		}
+
+		fmt.Printf("The replacement is %s\n", replacement)
+
+		//err = substitute(m, r.Target, replacement)
+		//if err != nil {
+		//	return err
+		//}
+	}
 	return nil
 }
+
 
--- kpt-functions/replacement/go.mod
+++ kpt-functions/replacement/go.mod
@@ -2,4 +2,7 @@ module github.com/aodinokov/noctl-airship-poc/kpt-functions/replacement
 
 go 1.14
 
-require sigs.k8s.io/kustomize/api v0.5.1
+require (
+	sigs.k8s.io/kustomize/api v0.5.1
+	sigs.k8s.io/kustomize/kyaml v0.4.1
+)
