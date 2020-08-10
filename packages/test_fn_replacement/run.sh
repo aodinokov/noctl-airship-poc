ac868df576f9f9c327db7a2b0103a13bf1088cfd
Alexey Odinokov <alexey.odinokov.82@gmail.com>
2020-08-10 11:39:36 -0500

    Update run.sh

--- packages/test_fn_replacement/run.sh
+++ packages/test_fn_replacement/run.sh
@@ -3,4 +3,4 @@ if [ ! -f ./kustomize ]; then
   curl -fsSL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.8.1/kustomize_v3.8.1_linux_amd64.tar.gz -o x.tar.gz && tar -zxvf x.tar.gz && rm x.tar.gz
 fi
 
-XDG_CONFIG_HOME=$(pwd)/manifests-example ./kustomize build --enable_alpha_plugins manifests-example/site/test-workload/target/workload/ > output.yaml
+KUSTOMIZE_PLUGIN_HOME=$(pwd)/manifests-example ./kustomize build --enable_alpha_plugins manifests-example/site/test-workload/target/workload/ > output.yaml
