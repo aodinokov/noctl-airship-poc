f7e19918f1e5e7f0a6de59a537d27d9db31529bf
Alexey Odinokov <mralex@mail.ru>
2020-08-25 00:20:31 +0000

    reused replacement function and filtering

--- kpt-functions/crypter/function.go
+++ kpt-functions/crypter/function.go
@@ -1,11 +1,9 @@
 package crypter
 
 import (
-	"bytes"
 	"fmt"
 	//"log"
 	"os"
-	"unicode/utf8"
 
 	"sigs.k8s.io/kustomize/kyaml/yaml"
 
@@ -73,65 +71,26 @@ func (f *Function) Exec(items []*yaml.RNode) ([]*yaml.RNode, error) {
 	}
 
 	for _, ref := range f.Config.Refs {
-		node, err := ref.ObjRef.FindOne(items)// TODO: FindAll!
+		nodes, err := ref.ObjRef.Filter(items)
 		if err != nil {
 			return nil, err
 		}
 
-		for _, fieldRef := range ref.FieldRefs {
-			err := f.execFieldOp(node, fieldRef)
-			if err != nil {
-				return nil, err
+		for _, node := range nodes {
+			for _, fieldRef := range ref.FieldRefs {
+				err := f.execFieldOp(node, fieldRef)
+				if err != nil {
+					return nil, err
+				}
 			}
 		}
 	}
 	return items, nil
 }
 
-// TODO: to get from replacement?
-func parseFieldRef(in string) ([]string, error) {
-	var cur bytes.Buffer
-	out := []string{}
-	var state int
-	for i := 0; i < len(in); {
-		r, size := utf8.DecodeRuneInString(in[i:])
-
-		switch state {
-		case 0: // initial state
-			if r == '.' {
-				if cur.String() != "" {
-					out = append(out, cur.String())
-					cur = bytes.Buffer{}
-				}
-			} else if r == '[' {
-				if cur.String() != "" {
-					out = append(out, cur.String())
-					cur = bytes.Buffer{}
-				}
-				cur.WriteRune(r)
-				state = 1
-			} else {
-				cur.WriteRune(r)
-			}
-		case 1: // state inside []
-			cur.WriteRune(r)
-			if r == ']' {
-				state = 0
-			}
-		}
-		i += size
-	}
-
-	if state != 0 {
-		return nil, fmt.Errorf("unclosed [")
-	}
-
-	return append(out, cur.String()), nil
-}
-
 func (f *Function) execFieldOp(node *yaml.RNode, fieldRef string) error {
 
-	p, err := parseFieldRef(fieldRef)
+	p, err := replacement.ParseFieldRef(fieldRef)
 	if err != nil {
 		return err
 	}
