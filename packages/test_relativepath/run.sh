8b50e8d7937893aa62356814e4aa82becb297445
Alexey Odinokov <mralex@mail.ru>
2020-12-10 00:39:49 +0000

    added test for https://github.com/GoogleContainerTools/kpt/issues/1151

---
+++ packages/test_relativepath/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ ! -f ./kustomize ]; then
+  curl -fsSL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.8.1/kustomize_v3.8.1_linux_amd64.tar.gz -o x.tar.gz && tar -zxvf x.tar.gz && rm x.tar.gz
+fi
+
+KUSTOMIZE_PLUGIN_HOME=$(pwd) ./kustomize build --as-current-user --network --enable_alpha_plugins manifests/ #> output.yaml
+
+
