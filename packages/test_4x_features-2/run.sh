98b8ae6928df289e98b650a984c145d48dc872b2
Alexey Odinokov <mralex@mail.ru>
2021-03-12 20:15:32 +0000

    testing layering with patches

---
+++ packages/test_4x_features-2/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ ! -f ./kustomize ]; then
+  curl -fsSL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.0.5/kustomize_v4.0.5_linux_amd64.tar.gz -o x.tar.gz && tar -zxvf x.tar.gz && rm x.tar.gz
+
+fi
+
+KUSTOMIZE_PLUGIN_HOME=$(pwd) ./kustomize build --enable-alpha-plugins $1
